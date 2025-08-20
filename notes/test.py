714c7139205308bbcd92185455f5412bed48a2cc
theone482 <iini.bohnet@ucas-edu.net>
2025-08-20 10:39:58 -0600

    i made a code that asks for your name and says hi afterwards

--- notes/test.py
+++ notes/test.py
@@ -1,2 +1,11 @@
+# IB 2nd input and outputs notes
 print("Test")
-#print() <= Everything inside the parenthesis apears in the terminal
+#print() <= Everything inside the parenthesis appears in the terminal
+color = input("Give me a color: ")
+print ("Your color is" , color)
+# Other option
+print("Tell me a color: ")
+color_two = input()
+print(color_two)
+
+#pick 1 method, do not change between them
---
+++ practices/hello_world.py
@@ -0,0 +1,4 @@
+# IB 2nd hello world 
+print("hello world")
+name = input("Nice to meet you but What is your name: ")
+print("Hi welcome to hello word" , name)

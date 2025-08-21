808373a5e783392e8bc7286f4cd38e7fbffcb536
theone482 <iini.bohnet@ucas-edu.net>
2025-08-21 10:09:48 -0600

    i did my seasons starter

---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+# IB 2nd starters season
+print ("season")
+lastname = input("what is your last name: ")
+season = input(" What is your favorite season: ")
+print("Wow" , lastname, "san, I like" , season, "too!")

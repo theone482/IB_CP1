3cf97b5516c7ec6d41bcfc04631886b177643ae1
theone482 <iini.bohnet@ucas-edu.net>
2025-08-22 10:41:30 -0600

    i did who are you and i did what was needed.

---
+++ practices/who_are_you.py
@@ -0,0 +1,5 @@
+# IB 2nd who are you
+name = input("what is your name: ")
+color = input("soooo what is your favorite color:")
+age = input("and your age is?: ")
+print("hi", name, "my favorite color is", color, "too. I am miss being", age, "again")

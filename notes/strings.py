498e3c120fe6b32699ef95cb39669d01a45c2786
theone482 <iini.bohnet@ucas-edu.net>
2025-09-02 10:51:14 -0600

    i finsihed my strings noptes

--- notes/strings.py
+++ notes/strings.py
@@ -1,2 +1,36 @@
-# IB 2md String notes
-S
+# IB 2nd Strings notes
+
+# ex
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = 'Xavier'
+month = 'september'
+food = "chocolate"
+Last_name = 'LaRose'
+
+# if i need to use ' or "
+name = "I'ini"
+best = '"The Return Of The King" is the best book ever!'
+
+
+funny = "the dog jumped into the lake"
+
+# Length of strings
+length = len(month)
+print("september has", length, "letter")
+
+# escape character
+ # escape = \ 
+print('\twill said: \n "Don\'t die"')
+
+
+#concatenate
+full_name = first_name + " " + Last_name
+
+print(full_name)
+
+# Index
+
+
+# slice string
+#string[start index, end index]
+print(Last_name[2:6])

e5e947514e291f589a473648972572dc929ac3d2
theone482 <iini.bohnet@ucas-edu.net>
2025-09-05 10:49:38 -0600

    i didn't get iut done bur egnough to do at home

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,4 +1,8 @@
 # IB 2nd idiot proof
-name = input("what is your full name: ")
-phone = input("And what is your number: ")
-gpa =  input("Soooo what is youe gpa :) : ")
+name = input("what is your full name: ").strip().title()
+phone = input("And what is your number: ").strip()
+print(phone.replace("/", " "))
+gpa =  float(input("Soooo what is your gpa? :) : \n "))
+print("Hi", name,"I'm emma")
+print("Your number is:", phone,)
+print(round(gpa, ))

3c4ad75dd179444d0f64db6123710b890a1fe1fa
theone482 <iini.bohnet@ucas-edu.net>
2025-09-09 10:51:07 -0600

    bvdjhvf

--- practices/basic_calculator.py
+++ practices/basic_calculator.py
@@ -2,7 +2,8 @@
 
 number_1 = float(input("give me a number: "))
 number_2 = float(input("give me a other different number: "))
-operations = input("pick one: +,-,**,*,/,: %")
-
-
-print("your gay")
+operations = input("pick one: +,-,**,*,/,: %: ")
+if operations == "+" :
+    print({number_1+number_2})
+else operations == "*" :
+    print({number_1*number_2})

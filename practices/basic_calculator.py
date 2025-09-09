0d0c4a41c0bd1903760d738936471e6f440041bd
theone482 <iini.bohnet@ucas-edu.net>
2025-09-09 17:00:20 -0600

    Update basic_calculator.py

--- practices/basic_calculator.py
+++ practices/basic_calculator.py
@@ -1,9 +1,20 @@
 # IB 2nd basic calculator
 
-number_1 = float(input("give me a number: "))
-number_2 = float(input("give me a other different number: "))
-operations = input("pick one: +,-,**,*,/,: %: ")
-if operations == "+" :
-    print({number_1+number_2})
-else operations == "*" :
-    print({number_1*number_2})
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
+
+addition = round(first_number + second_number, 2)
+subtraction = round(first_number - second_number, 2)
+multiplication = round(first_number * second_number, 2)
+
+if second_number != 0:
+    division = round(first_number / second_number, 2)
+else:
+    division = "undefined (can't divide by zero)"
+
+print("\nHere are your results:")
+print(f"{first_number} + {second_number} = {addition}")
+print(f"{first_number} - {second_number} = {subtraction}")
+print(f"{first_number} * {second_number} = {multiplication}")
+print(f"{first_number} / {second_number} = {division}")
+

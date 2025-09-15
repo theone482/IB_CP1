a508a74849dd053e4d8b5247aa426812a9761baa
theone482 <iini.bohnet@ucas-edu.net>
2025-09-15 10:50:19 -0600

    i did nothing

--- projects/Crew_shares.py
+++ projects/Crew_shares.py
@@ -1,5 +1,5 @@
 # IB 2nd crew shares 
-totalmoney = input("how much money did the crew earn in total: ")
-crew = input("how many crew members are there (not inculuding the captain and first mate): ")
-evening = input("how much money did the captian give the crew for the evening: ")
-moneyafterevening = round(totalmoney - evening, 2)
+totalmoney = float(input("how much money did the crew earn in total: "))
+crew = float(input("how many crew members are there (not inculuding the captain and first mate): "))
+
+

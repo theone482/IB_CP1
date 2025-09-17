4e3e140b5e006bab1d7a8918dc6944effe181264
theone482 <iini.bohnet@ucas-edu.net>
2025-09-16 20:54:10 -0600

    Update Crew_shares.py

--- projects/Crew_shares.py
+++ projects/Crew_shares.py
@@ -1,5 +1,16 @@
 # IB 2nd crew shares 
-totalmoney = float(input("how much money did the crew earn in total: "))
-crew = float(input("how many crew members are there (not inculuding the captain and first mate): "))
+
+total_earnings = float(input("How much money was earned in total? "))
+crew_members = int(input("How many crew members are there (excluding captain and first mate)? "))
+
+captain_share = total_earnings * 0.25 # Captain gets 25%
+first_mate_share = total_earnings * 0.15 # First mate gets 15%
+
+remaining_money = total_earnings - captain_share - first_mate_share
+
+if crew_members > 0:
+crew_share = remaining_money / crew_members
+else:
+crew_share = 0 # No crew to share with
 
 

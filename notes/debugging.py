69350ed5ed6299161c50e8543d84e24b3b4b9692
theone482 <iini.bohnet@ucas-edu.net>
2025-09-25 10:50:51 -0600

    i half notes

---
+++ notes/debugging.py
@@ -0,0 +1,8 @@
+# Ib 2nd debugging notes
+
+# Syntax error - grammar error
+# fix by => reading where the problem is AND fixing it!
+
+# Logic Error - The computer has the worng steps
+
+# Run-time Error

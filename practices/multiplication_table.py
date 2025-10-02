5fb87965a30276b901bd6031a730f604529b8908
theone482 <iini.bohnet@ucas-edu.net>
2025-10-02 10:47:18 -0600

    i did it

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -1,4 +1,4 @@
 # IB 2nd multiplication table
 
-table = [1,2,3,4,5,6,7,8,9,10]
-print(table)
+for i in range(0,16,1):
+    print(f"{i * 1} \t {i *2} \t {i * 3} \t {i * 4} \t {i * 5} \t {i * 6} \t {i * 7} \t {i * 8} \t {i * 9} \t {i * 10} \t {i * 11} \t {i * 12} \t {i * 13} \t {i * 14} \t {i * 15} \t ")

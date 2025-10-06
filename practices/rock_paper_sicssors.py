13a94238f17c8cecd0359be31c4339f30dc1fa46
theone482 <iini.bohnet@ucas-edu.net>
2025-10-06 10:50:52 -0600

    dsad

--- practices/rock_paper_sicssors.py
+++ practices/rock_paper_sicssors.py
@@ -1,13 +1,39 @@
 # Ib 2nd rock paper sicssors game
 import random
-print("1 is rock, 2 is paper and 3 is sicssors")
-player = input("what do you pick 1,2,3: ")
-ai = random.randint(1,3)
-if ai == 1:
-    print("The ai choose rock")
-if ai == 2:
-    print("the ai choose paper")
-if ai == 3:
-    print("The ai choose sicssors")
-if player == "1" and ai == 2:
-    print("the ai won")
+
+while True:
+    print("1 is rock, 2 is paper and 3 is sicssors and 4 is to quit game, 5 to show me the wins of th computer and me")
+    player = input("what do you pick 1,2,3,4,5: ")
+    ai = random.randint(1,3)
+    aiwons = 1
+    playerwons = 1
+    
+
+    if ai == 1 and player == "1" :
+        print("Tie try again")
+    elif ai == 2 and player == "1":
+        print("The Computer won")
+        aiwons += 1
+    elif ai == 3 and player == "1":
+        print("You won")
+        playerwons+= 1
+    elif ai == 1 and player == "2":
+        print("You won")
+        playerwons+= 1
+    elif ai == 2 and player == "2":
+        print("Tie try again")
+    elif ai == 3 and player == "2":
+        print("The computer won")
+        aiwons+= 1
+    elif ai == 1 and player == "3":
+        print("the computer won")
+        aiwons.append()
+    elif ai == 2 and player == "3":
+        print("you won")
+        playerwons.append()
+    elif ai == 3 and player == "3":
+        print("Tie try again")
+    if player == "5":
+        print(f" the computer wins:{aiwons} and player wins:{playerwons}")
+    if player == "4":
+        break
--- practices/tempCodeRunnerFile.py
+++ practices/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-your
+
+        playerwons += 1

be30619f953431191552dfb6c9c1d576ffc36001
theone482 <iini.bohnet@ucas-edu.net>
2025-10-20 10:50:53 -0600

    sfsf

---
+++ notes/functions.py
@@ -0,0 +1,16 @@
+# IB 2nd functions notes
+num = 0
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("duck")
+    num += 1
+print("goose")
+print(f"results is: {ass(-6519146,36516)}")
+total = add
+add(5,5)
+add(-6519146,36516)
+add(689461,654894)
+add(3.14,.85)
+add(42,7)

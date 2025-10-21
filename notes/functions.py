56dc9de3aff2d395c3a4453fb2bc14c928bcc6f2
theone482 <iini.bohnet@ucas-edu.net>
2025-10-21 10:50:52 -0600

    sdfdsf

--- notes/functions.py
+++ notes/functions.py
@@ -1,16 +1,49 @@
 # IB 2nd functions notes
+#all imports
+#set globel variables
 num = 0
+player_hp = 100
+monster_hp = 100
+
+# writer your functions
 def add(x,y):
     return x+y
 
+def initials(name):
+    names = name.split(" ")
+    initial = ""
+    for name in names:
+        initial += name[0]
+    return initial
+
+def attack(dmg, turn):
+    if turn == "player":
+        return monster_hp - dmg, player_hp
+    else:
+        return monster_hp, player_hp - dmg
+
+
+# write the rest of the code
 while num < add(5,5):
     print("duck")
     num += 1
 print("goose")
-print(f"results is: {ass(-6519146,36516)}")
-total = add
-add(5,5)
-add(-6519146,36516)
-add(689461,654894)
-add(3.14,.85)
+print(f"results is: {add(-6519146,36516)}")
+total = add(689461,654894)
+print(add(add(3.14,.85), 10))
 add(42,7)
+
+
+
+print(f"Tia's initials are: {initials("Tia LaRose")}")
+print(f"Xavier's initials are: {initials("Xavier LaRose")}")
+monster_hp, player_hp = attack(15, "monster")
+print(f"player health: {player_hp}")
+print(f"monster health: {monster_hp}")
+
+monster_hp, player_hp = attack(15, "player")
+print(f"player health: {player_hp}")
+print(f"monster health: {monster_hp}")
+
+print(f"a == {ord("a")}")
+print(f"100 = {chr(100)}")
---
+++ practices/Caeser_cipher.py
@@ -0,0 +1,3 @@
+# IB 2nd ceaser cipher
+message = input("what is your message?: ")
+choose = input("do you want your message encoding = 1  or decoding = 1: ")

647b0d66aaf251e66baa4cd397481a6e57d31fee
theone482 <iini.bohnet@ucas-edu.net>
2025-10-22 10:50:47 -0600

    vbcg

--- practices/Caeser_cipher.py
+++ practices/Caeser_cipher.py
@@ -1,3 +1,37 @@
 # IB 2nd ceaser cipher
-message = input("what is your message?: ")
-choose = input("do you want your message encoding = 1  or decoding = 1: ")
+
+# make it a loop
+while True:
+    # ask for message and ask if want encoded and decoded and ask how much they want the letters to move
+    message = input("what is your message?: ")
+    choose = input("do you want your message encoding = 1  or decoding = 2: ")  
+    shift = input("How much do you want the leters to move?: ")
+# make it look for te letters and see what are the three letters away 
+# id ti encoded make it more the letter three over
+#if they go passed z make it go back to A
+# if they want their message decoded then look
+# when done ask if they have snymore if they want encoded and decoded and if not break
+    "a == 97"
+    "b == 98"
+    "c == 99"
+    "d == 100"
+    "e == 101"
+    "f == 102"
+    "g == 103"
+    "h == 104"
+    "i == 105"
+    "j == 106"
+    "k == 107"
+    "l == 108"
+    "n"
+    "m"
+    "O"
+    "p"
+    "q"
+    "r"
+    "s"
+    "t"
+    "u"
+    "w"
+    "x"
+print(f"99 = {chr(99)}")

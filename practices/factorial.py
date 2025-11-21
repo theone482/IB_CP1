62583276ba65566e68521a5ae031e247c62e9d9e
theone482 <iini.bohnet@ucas-edu.net>
2025-11-21 10:50:54 -0700

    cscs

---
+++ practices/factorial.py
@@ -0,0 +1,29 @@
+#Ib 2nd factorial calculator
+
+# ask user for input"enter #" to make variable for original_num.
+original_num = input("enter a number: ")
+#check if user inputed int. # i changed the pseudocode by not checking if  og num can turn into float
+def is_interger(original_num):
+    check:
+        int(original_num)
+        return True
+    except valuerror:
+
+
+#i deleta a line that make  you use the num id it is a int as it is.
+#Except if you can't, turn it into a float print, "invalid, try again."
+# Definf factorial_calc()
+#while true
+def factorial_calc():
+    while True:
+        for i in range():
+
+
+    #for I in range()< make empthy list = total = num
+    #take the num var, and 0-1 it, and print so e.g: 3-1 2-1 1
+    #get it tinot a list.
+    #when list[num] == 0
+        #break
+
+    #product(list) or
+    #call factorial_calc
---
+++ starters/xscdc.py
@@ -0,0 +1,26 @@
+#Ib 2nd jfjf
+
+import turtle 
+
+
+def draw_branch(length):
+    if length > 5:
+        for i in range(3):
+            turtle.forward(10)
+            turtle.backward()
+            turtle.right(60)
+
+turtle.shape(turtle)
+turtle.speed(10)
+turtle.color("light blue")
+turtle.penup
+turtle.goto(100,50)
+turtle.pendown
+"""def main():
+    for i in range
+    set speed
+set cloor to light blue
+lift pen up
+memoryviewto startimg postionsput pen DeprecationWarning
+
+main"""

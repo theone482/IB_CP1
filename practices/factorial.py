c1b0f0a5cf2bd12a9211c7f1fae2da02ee6cf59c
theone482 <iini.bohnet@ucas-edu.net>
2025-11-25 03:50:50 +0000

    cdfvdc

--- practices/factorial.py
+++ practices/factorial.py
@@ -1,29 +1,42 @@
 #Ib 2nd factorial calculator
 
 # ask user for input"enter #" to make variable for original_num.
-original_num = input("enter a number: ")
+original_num = input("Enter a number: ")
+
+
 #check if user inputed int. # i changed the pseudocode by not checking if  og num can turn into float
-def is_interger(original_num):
-    check:
-        int(original_num)
+def is_integer(value):
+    try:
+        int(value)   # Try converting to int
         return True
-    except valuerror:
-
-
-#i deleta a line that make  you use the num id it is a int as it is.
-#Except if you can't, turn it into a float print, "invalid, try again."
+    except ValueError:   # If it fails, it's not an integer
+        return False
 # Definf factorial_calc()
-#while true
-def factorial_calc():
-    while True:
-        for i in range():
-
-
+def factorial_calc(num):
+    total = 1
+    for i in range(1, num + 1):
+        total *= i
+    return total
+    
+    
     #for I in range()< make empthy list = total = num
     #take the num var, and 0-1 it, and print so e.g: 3-1 2-1 1
     #get it tinot a list.
     #when list[num] == 0
         #break
+        #while true
+while True:
+    if is_integer(original_num):
+        num = int(original_num)
+        print("The factorial of", num, "is", factorial_calc(num))
+        break
+    else:
+        print("Invalid input, try again.")
+        original_num = input("Enter a number: ")
 
     #product(list) or
     #call factorial_calc
+factorial_calc()
+
+#  changed 'valuerror' to 'ValueError' because Python requires exact error names
+#  removed stray factorial_calc() call at the end since it needs an argument
